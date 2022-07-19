330f3e4692b47a3f247896032d6301a3ce3b0ca2
rkunamaneni <100539369+rajkunamaneni@users.noreply.github.com>
2022-07-19 14:52:56 -0700

    update and check again for second table

--- src/scraper.py
+++ src/scraper.py
@@ -6,103 +6,86 @@ from log import Logger
 def main():
     #set user info for mysql login
 
-    for i in range(0, 2):
-        #scrape the website 
-        url = "https://howpcrules.com/sample-page-for-web-scraping/"
+    url_to_scrape = 'https://howpcrules.com/sample-page-for-web-scraping/'
+    plain_html_text = requests.get(url_to_scrape)
+    soup = bsoup(plain_html_text.text, "html.parser")
+
+    #Get the name of the table
+    name_of_class = soup.h3.text.strip()
+
+    basic_data_table = soup.find("table", {"summary": "Basic data for the event"});
+   
+    basic_data_cells = basic_data_table.findAll('td')
+
+    type_of_course = basic_data_cells[0].text.strip()
+    lecturer = basic_data_cells[1].text.strip()
+    number = basic_data_cells[2].text.strip()
+    short_text = basic_data_cells[3].text.strip()
+    choice_term = basic_data_cells[4].text.strip()
+    hours_per_week_in_term = basic_data_cells[5].text.strip()
+    expected_num_of_participants = basic_data_cells[6].text.strip()
+    maximum_participants = basic_data_cells[7].text.strip()
+    assignment = basic_data_cells[8].text.strip()
+    lecture_id = basic_data_cells[9].text.strip()
+    credit_points = basic_data_cells[10].text.strip()
+    hyperlink = basic_data_cells[11].text.strip()
+    language = basic_data_cells[12].text.strip()
+
+    # Open database connection
+    db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    sql = "INSERT INTO classes(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, 'NOW()')
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
         
-        html = requests.get(url)
-        
-        parse = bsoup(html.text, "html.parser")
+    sql = "SELECT LAST_INSERT_ID()"
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        class_id = result[0]
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        db.close()
+        class_id = -1
 
-        name_of_class = parse.h3.text.strip()
+    #Get the tables where the dates are written.
+    dates_tables = soup.find_all("table", {"summary": "Overview of all event dates"});
 
-        table_contents = parse.find("table", {"summary": "Basic data for the event"});
-        findtd = table_contents.findAll('td')
+    for table in dates_tables:
+        for row in table.select("tr"):
+            cells = row.findAll("td")
+            if(len(cells) > 0):
+                duration = cells[0].text.split("to")
+                start_date = duration[0].strip()
+                end_date = duration[1].strip()
+                day = cells[1].text.strip()
+                time = cells[2].text.split("to")
+                start_time = time[0].strip()
+                end_time = time[1].strip()
+                frequency = cells[3].text.strip()
+                room = cells[4].text.strip()
+                lecturer_for_date = cells[5].text.strip()
+                status = cells[6].text.strip()
+                remarks = cells[7].text.strip()
+                cancelled_on = cells[8].text.strip()
+                max_participants = cells[9].text.strip()
+                #Save event data to database
 
-        #table content and filter
-        type_of_course = findtd[0].text.strip()
-        lecturer = findtd[1].text.strip()
-        number = findtd[2].text.strip()
-        short_text = findtd[3].text.strip()
-        choice_term = findtd[4].text.strip()
-        hours_per_week_in_term = findtd[5].text.strip()
-        expected_num_of_participants = findtd[6].text.strip()
-        maximum_participants = findtd[7].text.strip()
-        assignment = findtd[8].text.strip()
-        lecture_id = findtd[9].text.strip()
-        credit_points = findtd[10].text.strip()
-        hyperlink = findtd[11].text.strip()
-        language = findtd[12].text.strip()
-
-        db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
-
-        cursor = db.cursor()
-        sql = "INSERT INTO classes(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, 'NOW()')
-        try:
-            # fill each set of rows with data
-            cursor.execute(sql)
-            db.commit()
-
-        except:
-            # Rollback in case there is any error
-            db.rollback()
-            #get the just inserted class id
-            sql = "SELECT LAST_INSERT_ID()"
-        try:
-            cursor.execute(sql)
-            # Get the result
-            result = cursor.fetchone()
-            # Set the class id to the just inserted class
-            class_id = result[0]
-        except:
-            #error happened :(
-            db.rollback()
-            db.close()
-            class_id = -1
-            
-        dates_tables = parse.find_all("table", {"summary": "Overview of all event dates"});
-        #Iterate through the tables
-        for table in dates_tables:
-            #Iterate through the rows inside the table
-            for row in table.select("tr"):
-                #Get all cells inside the row
-                cells = row.findAll("td")
-                #check if there is at least one td cell inside this row
-                if(len(cells) > 0):
-                    #get all the different data from the table's tds
-                    #Split this cell into two different parts seperated by 'to' in order to have a start_date and an end_date.
-                    duration = cells[0].text.split("to")
-                    start_date = duration[0].strip()
-                    end_date = duration[1].strip()
-                    day = cells[1].text.strip()
-                    #Split this cell into two different parts seperated by 'to' in order to have a start_time and an end_time.
-                    time = cells[2].text.split("to")
-                    start_time = time[0].strip()
-                    end_time = time[1].strip()
-                    frequency = cells[3].text.strip()
-                    room = cells[4].text.strip()
-                    lecturer_for_date = cells[5].text.strip()
-                    status = cells[6].text.strip()
-                    remarks = cells[7].text.strip()
-                    cancelled_on = cells[8].text.strip()
-                    max_participants = cells[9].text.strip()
-                    #Save event data to database
-                    # Open database connection
-                    db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
-                    # prepare a cursor object using cursor() method
-                    cursor = db.cursor()
-                    # Prepare SQL query to INSERT a record into the database.
-                    sql = "INSERT INTO events(class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, 'NOW()')
-                    try:
-                    # Execute the SQL command
+                db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
+                cursor = db.cursor()
+                
+                sql = "INSERT INTO events(class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, 'NOW()')
+                try:
                     cursor.execute(sql)
-                    # Commit your changes in the database
                     db.commit()
-                    except:
-                    # Rollback in case there is any error
+                except:
                     db.rollback()
-                    # disconnect from server
-                    db.close()
+                db.close()
 
 if __name__ == "__main__":
     main()
