f6e496adc85d23461a633fd0a54edf8fe6d31fa5
Raj Kunamaneni <rkunaman@ucsc.edu>
2022-07-19 14:36:04 -0700

    added sql handling

--- src/scraper.py
+++ src/scraper.py
@@ -3,18 +3,9 @@ import requests
 import MySQLdb
 from log import Logger
 
-def connect():
-    #install mysql with "brew install mysql"
-    #start server with "brew services start mysql"
-    #end server with "brew services stop mysql"
+def main():
+    #set user info for mysql login
 
-    #tested with Sequel Pro
-    HOST = "localhost"
-    USERNAME = "test_user"
-    PASSWORD = ""
-    DATABASE = "test_sample"
-
-def request():
     #scrape the website 
     url = "https://howpcrules.com/sample-page-for-web-scraping/"
     
@@ -22,13 +13,13 @@ def request():
     
     parse = bsoup(html.text, "html.parser")
 
-    info = parse.h3.text.strip()
+    name_of_class = parse.h3.text.strip()
 
     table_contents = parse.find("table", {"summary": "Basic data for the event"});
     findtd = table_contents.findAll('td')
 
-    #table content 
-    course = findtd[0].text.strip()
+    #table content and filter
+    type_of_course = findtd[0].text.strip()
     lecturer = findtd[1].text.strip()
     number = findtd[2].text.strip()
     short_text = findtd[3].text.strip()
@@ -42,13 +33,39 @@ def request():
     hyperlink = findtd[11].text.strip()
     language = findtd[12].text.strip()
 
-    dates_table = read.find_all("table", {"summary": "Overview of all event dates"});
+    db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
+
+    cursor = db.cursor()
+    sql = "INSERT INTO classes(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, 'NOW()')
+    try:
+        # fill each set of rows with data
+        cursor.execute(sql)
+        db.commit()
+
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        #get the just inserted class id
+        sql = "SELECT LAST_INSERT_ID()"
+    try:
+        cursor.execute(sql)
+        # Get the result
+        result = cursor.fetchone()
+        # Set the class id to the just inserted class
+        class_id = result[0]
+    except:
+        #error happened :(
+        db.rollback()
+        db.close()
+        class_id = -1
+
+    dates_table = parse.find_all("table", {"summary": "Overview of all event dates"});
 
     #parse content of dates table
     for i in dates_table:
         for j in i.select("tr"):
             cell = j.findAll("td")
-            if (len(j) > 0):
+            if (len(cell) > 0):
                 #split the table
                 duration = cell[0].text.split("to")
                 start_date = duration[0].strip()
@@ -65,10 +82,20 @@ def request():
                 cancelled_on = cell[8].text.strip()
                 max_participants = cell[9].text.strip()
 
+                db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
+                # prepare a cursor object using cursor() method
+                cursor = db.cursor()
+                # Prepare SQL query to INSERT a record into the database.
+                sql = "INSERT INTO events(class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(class_id, start_date, end_date, day, start_time, end_time, frequency, room, lecturer_for_date, status, remarks, cancelled_on, max_participants, 'NOW()')
+                try:
+                    # Execute the SQL command
+                    cursor.execute(sql)
+                    # Commit your changes in the database
+                    db.commit()
+                except:
+                    #error happened :(
+                    db.rollback()
+                db.close()
 
-
-if __name__=='__main__':
-
-    connect()
-    request()
-
+if __name__ == "__main__":
+    main()
